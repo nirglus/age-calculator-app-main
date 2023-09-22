f825b37d0b5310121f15e083be3f451a9367a4e0
nirglus <nirgluskin@gmail.com>
2023-09-22 12:36:30 +0300

    change let to const

--- main.js
+++ main.js
@@ -8,16 +8,14 @@ arrowBtn.addEventListener("click", () => {
 });
 
 function calcAge(){
-    let birthDate = new Date(`${document.getElementById("year").value}-${document.getElementById("month").value}-${document.getElementById("day").value}`);
-    let currentDate = new Date();
-    let userYearAge = currentDate.getFullYear() - birthDate.getFullYear();
-    let userMonthAge = (userYearAge * 12 + currentDate.getMonth()) - birthDate.getMonth();
-    let userDayAge = Math.floor((currentDate - birthDate) / (24 * 60 * 60 * 1000));
+    const birthDate = new Date(`${document.getElementById("year").value}-${document.getElementById("month").value}-${document.getElementById("day").value}`);
+    const currentDate = new Date();
+    const userYearAge = currentDate.getFullYear() - birthDate.getFullYear();
+    const userMonthAge = (userYearAge * 12 + currentDate.getMonth()) - birthDate.getMonth();
+    const userDayAge = Math.floor((currentDate - birthDate) / (24 * 60 * 60 * 1000));
     if(currentDate.getMonth() < birthDate.getMonth()){
-        userYearAge = userYearAge - 1;
+        userYearAge--;
     }
-    console.log(birthDate);
-    console.log(userMonthAge);
     if(validateDay() && validateMonth() && validateYear()){
         document.getElementById("yearSpan").innerHTML = `${userYearAge}`;
         document.getElementById("monthsSpan").innerHTML = `${userMonthAge}`;
