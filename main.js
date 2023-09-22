51f45f9f3a057a8e8fd295b5436bbe90f68b164c
nirglus <nirgluskin@gmail.com>
2023-09-22 10:16:40 +0300

    calculate userDayAge

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ arrowBtn.addEventListener("click", () => {
     let currentDate = new Date();
     let userYearAge = currentDate.getFullYear() - birthDate.getFullYear();
     let userMonthAge = (userYearAge * 12 + currentDate.getMonth()) - birthDate.getMonth();
+    let userDayAge = Math.floor((currentDate - birthDate) / (24 * 60 * 60 * 1000));
     if(currentDate.getMonth() < birthDate.getMonth()){
         userYearAge = userYearAge - 1;
     }
@@ -10,5 +11,5 @@ arrowBtn.addEventListener("click", () => {
     console.log(userMonthAge);
     document.getElementById("yearSpan").innerHTML = `${userYearAge}`;
     document.getElementById("monthsSpan").innerHTML = `${userMonthAge}`;
-    document.getElementById("daysSpan").innerHTML = `${userDay}`;
+    document.getElementById("daysSpan").innerHTML = `${userDayAge}`;
 })
