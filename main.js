2e246cf396b7efa8c62e223f71f1c6f1c957584a
nirglus <nirgluskin@gmail.com>
2023-09-22 09:02:46 +0300

    add calculation for age

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 arrowBtn.addEventListener("click", () => {
-    let userDay = day.value;
-    let userMonth = month.value;
-    let userYear = year.value;
+    let birthDate = new Date(`${year.value}-${month.value}-${day.value}`);
+    let currentDate = new Date();
+    let userYearAge;;
+    if(currentDate.getMonth() < birthDate.getMonth()){
+        userYearAge = currentDate.getFullYear() - birthDate.getFullYear() - 1;
+    }else{
+        userYearAge  = currentDate.getFullYear() - birthDate.getFullYear();
+    }
+    let userMonthAge = (currentDate.getMonth() - birthDate.getMonth()) * userYearAge;
+    console.log(birthDate);
+    console.log(userMonthAge);
+    document.getElementById("yearSpan").innerHTML = `${userYearAge}`;
+    document.getElementById("monthSpan").innerHTML = `${userMonth}`;
+    document.getElementById("daySpan").innerHTML = `${userDay}`;
 })
