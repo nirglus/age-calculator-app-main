80b1031ef8ca89a0e5ca2bb0fc0d3e84c7846db2
nirglus <nirgluskin@gmail.com>
2023-09-23 12:30:01 +0300

    add evenet listener for enter key

--- main.js
+++ main.js
@@ -88,3 +88,8 @@ function validateAndCalc(){
     }
 }
 arrowBtn.addEventListener("click", validateAndCalc);
+document.addEventListener("keydown", (event) =>{
+    if(event.key == "Enter"){
+        validateAndCalc();
+    }
+})
